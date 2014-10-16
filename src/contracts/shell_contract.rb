04d46fb97672e515f0edf93b1ec5211631f86e7c
Isaac Supeene <isupeene@ualberta.ca>
2014-10-16 15:39:24 -0600

    Command syntax definition.

--- src/contracts/shell_contract.rb
+++ src/contracts/shell_contract.rb
@@ -2,6 +2,13 @@ require 'test/unit'
 require_relative '../shell'
 
 class ShellContract < Test::Unit::TestCase
+	# Extremely simple definition of valid command.
+	DOUBLE_QUOTED_WORD = /"[^"\n[[:cntrl:]]]*"/
+	SINGLE_QUOTED_WORD = /'[^'\n[[:cntrl:]]]*'/
+	PLAIN_WORD = /[\w-]*/
+	WORD = /#{PLAIN_WORD}|#{DOUBLE_QUOTED_WORD}|#{SINGLE_QUOTED_WORD}/
+	VALID_COMMAND = /^ *#{WORD}( +#{WORD})*$/
+
 	def test_execute_command
 		# Setup
 		shell_input_read, shell_input_write = IO.pipe()
@@ -11,7 +18,7 @@ class ShellContract < Test::Unit::TestCase
 		command = "ls -l"
 
 		# Preconditions
-		assert_match("", command, "Command was invalid") # TODO
+		assert_match(VALID_COMMAND, command, "Command was invalid")
 
 		# Function
 		shell_input_write.write(command)
