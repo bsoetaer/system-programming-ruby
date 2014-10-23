a6df22c4b8feda1edf8211dff267fb6d3d95053f
Isaac Supeene <isupeene@ualberta.ca>
2014-10-23 12:07:01 -0600

    Functional-style implementation of split.

--- src/shell.rb
+++ src/shell.rb
@@ -1,5 +1,12 @@
 require 'shell'
 
+class Array
+	def self.unfold_from(seed, &block)
+		value, seed = yield seed
+		return seed ? [value, *unfold_from(seed, &block)] : [value]
+	end
+end
+
 class Shell421 < Shell
 	DOUBLE_QUOTED_WORD = /"[^"\n[[:cntrl:]]]*"/
 	SINGLE_QUOTED_WORD = /'[^'\n[[:cntrl:]]]*'/
@@ -16,18 +23,18 @@ class Shell421 < Shell
 					next if input =~ /^\s*$/
 					raise Exception unless input =~ VALID_COMMAND
 
-					command, args = split(input)
-					if respond_to?(command)
-						puts("#{command}, #{args}")
-						output_pipe.write(send(command, *args).to_s + "\n")
+					args = split(input)
+					if respond_to?(args[0])
+						output_pipe.write(send(*args).to_s + "\n")
 					else
 						fork {
-							exec(command, *args, {:in => input_pipe, :out => output_pipe})
+							exec(*args, {:in => input_pipe, :out => output_pipe})
 						}
 					end
 				rescue EOFError
 					break
-				rescue Exception
+				rescue Exception => e
+					puts e
 					puts "error" # TODO: Error handling
 				end
 			end
@@ -38,14 +45,9 @@ class Shell421 < Shell
 
 	private
 	def split(input)
-		command = VALID_COMMAND.match(input)[1]
-		input = VALID_COMMAND.match(input)[2]
-		args = []
-		while input.length > 0
-			args << VALID_COMMAND.match(input)[1]
-			input = VALID_COMMAND.match(input)[2]
-		end
-		return command, args
+		Array.unfold_from(input) { |seed|
+			VALID_COMMAND.match(seed)[1..2].select{ |x| x !~ /^\s*$/ }
+		}
 	end
 end
 
