b8df0e9f79d587bf030580ffa932ae0abc1d6a94
bsoetaer <bsoetaer@ualberta.ca>
2014-10-22 19:37:00 -0700

    Added error exceptions when nanosleep fails. Also made some review changes.

--- src/delayed_action.rb
+++ src/delayed_action.rb
@@ -1,27 +1,27 @@
 require_relative "Delay"
 
 module DelayedAction 
-	def self.delayed_action(sec, nsec, *args)
+	def self.delayed_action(sec, nsec)
 		# TODO: Do this without fork somehow.
 		child = fork do
 			code = Delay.nanodelay(sec,nsec)
 			unless code == Errno::NOERROR::Errno
 				error_code code
 			end
-			yield *args
+			yield 
 		end
 		Process.detach(child)
 	end
 	
 	def self.delayed_print(sec, nsec, msg)
-		delayed_action(sec, nsec, msg) { |m|
-			puts m
+		delayed_action(sec, nsec) { 
+			puts msg
 		}
 	end
 	
 	private
 	def self.error_code (code)
-		# TODO: Figure out how to change a number into an Errno class
-		#Errno.constants.select{|i| Errno::i::Errno == code}.each{|i| puts i}
+		error = Errno.constants.find{|i| Errno.const_get(i)::Errno == code}
+		raise Errno.const_get(error)
 	end
 end
